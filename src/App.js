a07b13a088b9167c619c6a51ca6a3a594ddb90c8
ADITINAMDEO13 <aditinamdeo131199@gmail.com>
2021-02-12 19:01:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import City from './component/City';
+import CityWheather from './component/CityWheather';
+
+
 
 function App() {
+
+  
+  const [cityName , setCityName] = React.useState("");
+  const [cityWeather , setCityWeather] = React.useState([]);
+  const fetchCityWeather = () =>{
+    console.log("I am from fetchCityWeather ");
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`)
+    .then((res) =>  res.json() )
+    .then((result)=> {
+      console.log("Result is ",result);
+      setCityWeather( {
+          data : result
+        }
+        );
+    }
+    
+    ) ;
+    
+  }; 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <City cityName={cityName} setCityName={setCityName} fetchCityWeather={fetchCityWeather} />
+        
+        {
+
+          cityWeather.data != undefined ?
+          
+          <div>
+            <CityWheather weather={cityWeather.data}/>
+          </div>
+          : null
+
+        }
+
+    </>
   );
 }
 
---
+++ src/component/CityWheather/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import './style.css';
+
+const CityWheather = (weather) => {
+    
+    return (
+        <div>
+            <div className="box">
+                <div> <span>City Name </span> =  {weather.weather.name} </div>
+                <div> <span>Wind Speed </span> = {weather.weather.wind.speed}  </div>
+                <div> <span>humidity </span> = {weather.weather.main.humidity}</div>
+                <div> <span>Temprature </span> = { Math.floor(weather.weather.main.temp -273.15)} <sup>o</sup> C </div>
+                <div> <span>Max-Temprature </span> = { Math.floor(weather.weather.main.temp_max -273.15)} <sup>o</sup> C </div>
+                <div> <span>Min-Temprature </span> = { Math.floor(weather.weather.main.temp_min -273.15)} <sup>o</sup> C </div>
+            </div>
+        </div>
+    );
+};
+
+export default CityWheather;
