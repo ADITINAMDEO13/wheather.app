6c1b3c272a59565751b9f73db25c5ed370c974bf
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-04-14 01:29:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,72 @@
-import React from 'react';
+import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import City from './component/City';
-import CityWheather from './component/CityWheather';
-
-
+import "./App.css";
+import City from "./component/City";
+import CityWheather from "./component/CityWheather";
 
 function App() {
-
-  
-  const [cityName , setCityName] = React.useState("");
-  const [cityWeather , setCityWeather] = React.useState([]);
-  const fetchCityWeather = () =>{
+  const [cityName, setCityName] = React.useState("");
+  const [cityWeather, setCityWeather] = React.useState([]);
+  const fetchCityWeather = () => {
     console.log("I am from fetchCityWeather ");
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`)
-    .then((res) =>  res.json() )
-    .then((result)=> {
-      console.log("Result is ",result);
-      setCityWeather( {
-          data : result
-        }
-        );
+    //   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`)
+    //   .then((res) =>  res.json() )
+    //   .then((result)=> {
+    //     console.log("Result is ",result);
+    //     setCityWeather( {
+    //         data : result
+    //       }
+    //       );
+    //   }
+
+    //   ) ;
+
+    // };
+
+    if (window.location.protocol === "http:") {
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((result) => {
+          // console.log("Result is ",result);
+          setCityWeather({
+            data: result
+          });
+        });
+    } else {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4deb3d5b5a9ba1475a3e91881ff30ce9`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((result) => {
+          // console.log("Result is ",result);
+          setCityWeather({
+            data: result
+          });
+        });
     }
-    
-    ) ;
-    
-  }; 
+  };
+
   return (
-    <>
-        <City cityName={cityName} setCityName={setCityName} fetchCityWeather={fetchCityWeather} />
-        
-        {
+    <div className="main-container">
+      <City
+        cityName={cityName}
+        setCityName={setCityName}
+        fetchCityWeather={fetchCityWeather}
+      />
 
-          cityWeather.data != undefined ?
-          
-          <div>
-            <CityWheather weather={cityWeather.data}/>
-          </div>
-          : null
-
-        }
-
-    </>
+      {cityWeather.data !== undefined ? (
+        <div>
+          <CityWheather weather={cityWeather.data} />
+        </div>
+      ) : null}
+    </div>
   );
 }
 
