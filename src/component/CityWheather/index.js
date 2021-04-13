9642cc465d76da5b5e301c6f1c2e735719ee4256
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-04-14 01:31:12 +0530

    Update index.js

--- src/component/CityWheather/index.js
+++ src/component/CityWheather/index.js
@@ -1,21 +1,38 @@
-
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 const CityWheather = (weather) => {
-    
-    return (
-        <div>
-            <div className="box">
-                <div> <span>City Name </span> =  {weather.weather.name} </div>
-                <div> <span>Wind Speed </span> = {weather.weather.wind.speed}  </div>
-                <div> <span>humidity </span> = {weather.weather.main.humidity}</div>
-                <div> <span>Temprature </span> = { Math.floor(weather.weather.main.temp -273.15)} <sup>o</sup> C </div>
-                <div> <span>Max-Temprature </span> = { Math.floor(weather.weather.main.temp_max -273.15)} <sup>o</sup> C </div>
-                <div> <span>Min-Temprature </span> = { Math.floor(weather.weather.main.temp_min -273.15)} <sup>o</sup> C </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="details-container">
+      <div>
+        {" "}
+        <span>City Name </span> = {weather.weather.name}{" "}
+      </div>
+      <div>
+        {" "}
+        <span>Wind Speed </span> = {weather.weather.wind.speed}{" "}
+      </div>
+      <div>
+        {" "}
+        <span>humidity </span> = {weather.weather.main.humidity}
+      </div>
+      <div>
+        {" "}
+        <span>Temprature </span> ={" "}
+        {Math.floor(weather.weather.main.temp - 273.15)} <sup>o</sup> C{" "}
+      </div>
+      <div>
+        {" "}
+        <span>Max-Temprature </span> ={" "}
+        {Math.floor(weather.weather.main.temp_max - 273.15)} <sup>o</sup> C{" "}
+      </div>
+      <div>
+        {" "}
+        <span>Min-Temprature </span> ={" "}
+        {Math.floor(weather.weather.main.temp_min - 273.15)} <sup>o</sup> C{" "}
+      </div>
+    </div>
+  );
 };
 
 export default CityWheather;
