23a244da13e0dd9c1f1acaa9598e5b0c68d80d5b
ADITINAMDEO13 <68394896+ADITINAMDEO13@users.noreply.github.com>
2021-04-14 01:30:09 +0530

    Update index.js

--- src/component/City/index.js
+++ src/component/City/index.js
@@ -1,44 +1,61 @@
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import "./style.css";
 
-import React from 'react';
-import {Form, Button, Container} from 'react-bootstrap';
+const City = ({ cityName, setCityName, fetchCityWeather }) => {
+  const [error, setError] = React.useState("");
 
+  const handleInputChange = (event) => {
+    setCityName(event.target.value);
+  };
 
-const City = ({ cityName , setCityName, fetchCityWeather }) => {
-    
-    const [error , setError] = React.useState("");
-
-    
-    const handleInputChange =(event)=>{
-            setCityName(event.target.value);
+  const handleClick = () => {
+    if (!cityName) {
+      setError("City Feild is empty");
+    } else {
+      setError("");
+      fetchCityWeather();
     }
+  };
 
-    const handleClick = () =>{
-        if(!cityName){
-            setError("City Feild is empty"); 
-        }
-        else{
-            setError("");
-            fetchCityWeather();
-        }
-    } 
+  // console.log("City Name is : ",cityName);
 
-    // console.log("City Name is : ",cityName);
+  return (
+    <>
+      {/* <Container>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={cityName}
+              type="text"
+              placeholder="Enter City Name"
+              onChange={handleInputChange}
+            />
+            <p className="text-danger">{error}</p>
+          </Form.Group>
 
-    return (
-        <Container>
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control value={cityName} type="text" placeholder="Enter City Name" onChange={handleInputChange}/>
-                    <p className="text-danger">{error}</p>
-                </Form.Group>
+          <Button variant="primary" onClick={handleClick}>
+            Submit
+          </Button>
+        </Form>
+      </Container> */}
 
-                <Button variant="primary" onClick={handleClick}>
-                    Submit
-                </Button>
-            </Form>
-        </Container>
-    );
+      <div className="city-container">
+        {/* <label className="lable-text">City</label> */}
+        <input
+          value={cityName}
+          type="text"
+          placeholder="Enter City Name"
+          onChange={handleInputChange}
+        />
+        <p className="text-danger">{error}</p>
+        <button className="bttn" onClick={handleClick}>
+          Submit
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default City;
